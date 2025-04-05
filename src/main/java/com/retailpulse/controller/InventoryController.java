6ed10cc08d34cc0ed003a2c61da207fb02b6db64
Galvin <galvinleow@gmail.com>
2025-04-05 14:12:35 +0800

    SCRUM-141 Change in backend

--- src/main/java/com/retailpulse/controller/InventoryController.java
+++ src/main/java/com/retailpulse/controller/InventoryController.java
@@ -2,7 +2,6 @@ package com.retailpulse.controller;
 
 import com.retailpulse.entity.Inventory;
 import com.retailpulse.service.InventoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
